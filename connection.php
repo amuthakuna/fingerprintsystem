752d8c4c68790ce57ffc2b37af2e05082000ae7e
amuthakuna <67719052+amuthakuna@users.noreply.github.com>
2021-01-22 05:43:41 +0530

    Add files via upload
    database connection file

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php 
+$con = mysqli_connect('localhost', 'root','','projectattendancedb');
+
+
+	date_default_timezone_set("Asia/Colombo");
+	
+	//$result = mysqli_query($con);
+	/*if(date("l")=="Sunday")
+	{
+		$query="UPDATE `timetable` SET `chck`='no'";
+	$result = mysqli_query($con,$query);
+	}*/
+	
+ ?>
